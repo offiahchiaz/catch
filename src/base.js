1635f3b9ce446de44f199b4d2e1acd6ed4464fbc
offiahchiaz <offiahchiaz@gmail.com>
2018-01-30 15:01:38 +0100

    Added Files

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyA0-VwmFwBGmhpekTRsr5TG8C9t3IJmmLM",
+    authDomain: "catch-chiaz.firebaseapp.com",
+    databaseURL: "https://catch-chiaz.firebaseio.com",
+});
+
+export default base;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Header = (props) => {
+
+    return (
+        <header className="top">
+            <h1>
+                Catch
+                <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                Day
+            </h1>
+            <h3 className="tagline"><span>{props.tag}</span></h3>
+        </header>
+    )
+};
+
+Header.propTypes = {
+    tag: React.PropTypes.string.isRequired
+};
+
+
+export default Header;
